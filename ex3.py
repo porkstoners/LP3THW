b3776d3a2efc4b1bfd945a39eeca3dc7da3b476d
Douglas Barker <dbarker@HRLD.COM>
2019-10-02 15:56:04 -0400

    created file ex3.py for the course.

---
+++ ex3.py
@@ -0,0 +1,9 @@
+# A comment, this is so you can read your program laters.
+# Anything after the # is ignored by python
+
+print("I could have a code like this.") # and the comment after is ignored 
+
+# You can alos use a comment to "disable" or comment out code:
+# print("This will not run")
+
+print("This will run")
